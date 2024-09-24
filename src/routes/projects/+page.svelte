62a80cbd391577cf3de0fb690aeed1a7b477b1c9
Siddarth S <schidsteradinson@gmail.com>
2024-09-25 02:08:00 +0530

    backup

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,17 @@
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<h2 onclick={() => (window.location.href = 'https://decalculae.vercel.app')} class="font-sans px-6 text-neutral-200 w-fit">
+	🧮 <span class="underline underline-dotted underline-offset-4">DeCalculaè</span>
+</h2>
+<p class="font-sans px-6 text-neutral-400 tracking-wider line-height-relaxed">A really good looking calculator.</p>
+<embed src="https://decalculae.vercel.app" class="w-full overflow-x-clip" />
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 <h2 onclick={() => (window.location.href = 'https://suchi-colors.netlify.app')} class="font-sans px-6 text-neutral-200 w-fit">
 	🎨 <span class="underline underline-dotted underline-offset-4">Color Scroll</span>
 </h2>
 <p class="font-sans px-6 text-neutral-400 tracking-wider line-height-relaxed">
 	Tired of scrolling videos all day? Become more tired by mindlessly scrolling infinite colors! Try scolling below
 </p>
-<embed src="http://suchi-colors.netlify.app" class="w-full overflow-x-clip" />
-
-<button>hello</button>
+<embed src="https://suchi-colors.netlify.app" class="w-full overflow-x-clip" />
