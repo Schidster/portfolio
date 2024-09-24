74072d9a250854667d73da4ff1f79c2bd115c12e
Siddarth S <schidsteradinson@gmail.com>
2024-09-25 02:12:38 +0530

    backup

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,7 +4,7 @@
 	🧮 <span class="underline underline-dotted underline-offset-4">DeCalculaè</span>
 </h2>
 <p class="font-sans px-6 text-neutral-400 tracking-wider line-height-relaxed">A really good looking calculator.</p>
-<embed src="https://decalculae.vercel.app" class="w-full overflow-x-clip" />
+<embed src="https://decalculae.vercel.app" class="w-full overflow-x-clip h-110" />
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
