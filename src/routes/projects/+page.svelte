b7eb8d1cb6dca8b693265defff3150234a044148
Siddarth S <schidsteradinson@gmail.com>
2024-09-25 17:16:27 +0530

    backup

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -8,6 +8,24 @@
 		src="https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FSchidster%2Fhomespan%2Fblob%2Fmain%2Fhomespan.ino%23L6-L18&style=mono-blue&type=code&showBorder=on&showLineNumbers=on&showFileMeta=on"
 	></script>
 </svelte:head>
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<h2 onclick={() => (window.location.href = 'https://complaints.vercel.app/feed/all')} class="font-sans px-6 text-neutral-200 w-fit">
+	💥 <span class="underline underline-dotted underline-3 underline-offset-4" style="text-decoration-color: {$accentColor};">Complaints</span
+	>
+</h2>
+<p class="font-sans px-6 text-neutral-400 tracking-wider line-height-relaxed">
+	An anonymous platform where people can complain about what they don't like and others can rant about it!
+</p>
+<button
+	onclick={() => (window.location.href = 'https://complaints.vercel.app')}
+	class="font-mono font-bold ml-6 px-2 py-1 mb-4 b-4 text-neutral-2 rounded-md bg-dark-5"
+	style="border-color: {$accentColor}">VIEW PROJECT</button
+>
+<embed src="https://complaints.vercel.app/feed/all" class="w-full overflow-x-clip h-110" />
+<div class="h-6"></div>
+
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 <h2 onclick={() => (window.location.href = 'https://decalculae.vercel.app')} class="font-sans px-6 text-neutral-200 w-fit">
